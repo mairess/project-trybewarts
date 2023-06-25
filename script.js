f0367f122d002e40a112dc1d9f4d703c91f9fe80
Matheus Domingues <matheusdominguesduartep@hotmail.com>
2023-06-25 10:49:24 -0300

    feat: switch Form teste

--- script.js
+++ script.js
@@ -28,8 +28,30 @@ function charCount() {
 
 textarea.addEventListener('input', charCount);
 
+// const switchForm = () => {
+//   const valueName = document.getElementById('input-name').value;
+//   const form = document.getElementById('evaluation-form');
+//   const newDiv = document.createElement('div');
+//   const mainForm = document.getElementById('main-form');
+//   newDiv.id = 'form-data';
+
+//   form.innerHTML = '';
+
+//   const newParagrafoName = document.createElement('p');
+//   newParagrafoName.innerHTML = `Nome: ${valueName}`;
+//   newDiv.appendChild(newParagrafoName);
+//   // mainForm.appendChild(newDiv);
+
+//   const valueEmail = document.getElementById('input-email').value;
+//   const newParagrafoEmail = document.createElement('p');
+//   newParagrafoEmail.innerHTML = `Email: ${valueEmail}`;
+//   newDiv.appendChild(newParagrafoEmail);
+//   mainForm.appendChild(newDiv);
+// };
+
 window.onload = () => {
   submitBtn.disabled = true;
   submitBtnHeader.addEventListener('click', submitAlert);
   agreement.addEventListener('change', verifyCheckbox);
+  // submitBtn.addEventListener('click', switchForm);
 };
