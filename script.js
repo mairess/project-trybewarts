6ea9cf61fb49316a53d04ad36fb5ce1095392c78
Maires Souza <trabalho.maires@gmail.com>
2023-06-28 11:14:15 -0300

    style: footer

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const submitBtnHeader = document.getElementById('submitBtnHeader');
 const submitBtn = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
-
 function verifyCheckbox() {
   submitBtn.disabled = !agreement.checked;
 }
-
 const submitAlert = () => {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
@@ -17,17 +15,13 @@ const submitAlert = () => {
 };
 const textarea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
-
 const charLimit = 500;
-
 function charCount() {
   const inputTextareaLength = textarea.value.length;
   const remainig = charLimit - inputTextareaLength;
   counter.textContent = remainig;
 }
-
 textarea.addEventListener('input', charCount);
-
 // trye switch
 // const switchForm = () => {
 //   const valueName = document.getElementById('input-name').value;
@@ -35,21 +29,17 @@ textarea.addEventListener('input', charCount);
 //   const newDiv = document.createElement('div');
 //   const mainForm = document.getElementById('main-form');
 //   newDiv.id = 'form-data';
-
 //   form.innerHTML = '';
-
 //   const newParagrafoName = document.createElement('p');
 //   newParagrafoName.innerHTML = `Nome: ${valueName}`;
 //   newDiv.appendChild(newParagrafoName);
 //   // mainForm.appendChild(newDiv);
-
 //   const valueEmail = document.getElementById('input-email').value;
 //   const newParagrafoEmail = document.createElement('p');
 //   newParagrafoEmail.innerHTML = `Email: ${valueEmail}`;
 //   newDiv.appendChild(newParagrafoEmail);
 //   mainForm.appendChild(newDiv);
 // };
-
 window.onload = () => {
   submitBtn.disabled = true;
   submitBtnHeader.addEventListener('click', submitAlert);
